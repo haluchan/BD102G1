708cf874ebd58e12d5debb6f75d56bb90f774c2c
Halu <forchang0120@gmail.com>
2017-09-29 19:36:05 +0800

    init

---
+++ php/0614-tryPHP.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>TryPHP</title>
+</head>
+<body>
+    <?php
+    echo "Can you hear me?";
+    ?>
+
+    <?
+    echo "or you can do this way";
+	echo "on the stage";
+    ?>
+</body>
+</html>
