75696765beb88147c3271f491dbf35532e14169c
pon <bd102pon@gmail.com>
2017-10-11 18:53:50 +0800

    都市小農-未套公版

--- js/fish.js
+++ js/fish.js
@@ -3,45 +3,109 @@ $(function(){
   var controller = new ScrollMagic.Controller();
  
   //tweenmax.fromto(目標物件,時間)用法
-  //Title飛入
-   var title= TweenMax.fromTo( ".fish_universeTitle" , 1.5, {
-        x : -300,     
+   //火箭飛入
+   var rocket= TweenMax.fromTo("#rocket", 1, {
+    x : 500,     
         opacity : 0,
     }, {
        x : 0,
        opacity : 1,
-       ease :  Bounce.easeOut
+       ease : Elastic.easeOut
     });
 
 
 
-  //找到parallax  newParallax
-  var scene_star = document.getElementById('star')
-  var parallax = new Parallax(scene_star)
+
+  //Title煙霧飛入
+ //   var title= TweenMax.fromTo( ".fish_universeTitle" , 2, {
+ //        // transformOrigin:"right 5%",
+ //        scaleX: 0.2,
+ //        scaleY: 0.2,
+ //        x :500, 
+ //        y:-100,    
+ //        opacity : 0
+ //    }, {
+ //       scaleX: 1,
+ //       scaleY: 1,
+ //       x : 0,
+ //       y:0,
+ //       opacity : 1,
+ //       ease :  Bounce.easeOut
+ //    });
+
+ // title.delay(0.5)
 
 
-  //觸發地球變大
-    var scene_big = new ScrollMagic.Scene({
-              
-              triggerElement: "#trigger1",
-              offset: -450
-            
-          })
-          .setClassToggle(' #earth','big' )
-          // .addIndicators({
-          //     name: 'big'
-          // })
-          .addTo(controller);
+
+
+
+//parallax  
+var scene_star = document.getElementById('star')
+var parallax = new Parallax(scene_star)
+
+
+
+// //選購流程1-scrollMagic
+// var stepl = TweenMax.staggerFromTo(['.stepFlowLeft #one','.stepFlowLeft #two','.stepFlowLeft #three'] ,1,{
+//   y : -500,
+//   opacity : 0,
+// },{
+//    y : 0,
+//   opacity:1
+// },0.5);
+
+// var scene = new ScrollMagic.Scene({
+      
+//       triggerElement: "#trigger1",
+//       duration: 300,
+//       offset: 10,
+//       reverse: false
+//     })
+
+//     .setTween(stepl)
+//     .addIndicators({
+//             name: 'scene 01'
+//         })
+// .addTo(controller);
+
+
+
+
+
+//選購流程2-scrollMagic
+var stepr = TweenMax.staggerFromTo(['.stepFlowRight #arrow1','.stepFlowRight #arrow2','.stepFlowRight #arrow3'],0.5,{
+  opacity:0
+},{
+  opacity:1
+},0.5);
+
+var scene = new ScrollMagic.Scene({
+      
+      triggerElement: "#trigger2",
+      duration: 300,
+      offset: 10,
+      reverse: false
+      
+    })
+
+    .setTween(stepr)
+    .addIndicators({
+            name: 'scene 02'
+        })
+.addTo(controller);
+
+
+
+
+
+
+
 
 
 
 
 
 })
-
-
-
-
 
 
 
