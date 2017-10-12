1e7b4ae9e434906926e7560f3c984917adf3a131
許謦懷 <beck_hsu@Becks-MacBook-Pro.local>
2017-10-12 19:31:04 +0800

    aa

---
+++ Footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+	<script src="js/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="CSS/headerFooter.css">
+	
+</head>
+<body>
+	<footer class="col-xs-12">
+			<div class="backgroundimg">
+
+				<div class="footerTitle">
+					<img src="src/image/hopeFarm/footer-title.png">
+				</div>
+				<div class="footerItemList">
+					<ul>
+						<li><a href=""><h3>希望農場</h3></a></li>
+						<li><a href=""><h3>體驗活動</h3></a></li>
+						<li><a href=""><h3>都市小農</h3></a></li>
+						<li><a href=""><h3>紅利菜園</h3></a></li>
+					</ul>
+					<ul>
+						<li><a href=""><h3>會員專區</h3></a></li>
+						<li><a href=""><h3>申請資助</h3></a></li>
+						<li><a href=""><h3>查詢案件</h3></a></li>
+						<li><a href=""><h3>回報進度</h3></a></li>
+					</ul>
+					<ul>
+						<li><a href=""><h3>聯絡我們</h3></a></li>
+						<li><a href=""><h3>03-4257387</h3></a></li>
+						<li><a href=""><h3>320 桃園市中壢區中大路300號</h3></a></li>
+					</ul>
+			  </div>
+			  <div class="footerIcon">
+					<a href=""><img src="src/image/hopeFarm/footer-icon01.png"></a>
+					<a href=""><img src="src/image/hopeFarm/footer-icon02.png"></a>
+					<a href=""><img src="src/image/hopeFarm/footer-icon03.png"></a>
+					
+			  </div>
+			  <div class="clear"></div>
+			</div>
+	</footer>
+
+<script type="text/javascript" src="js/NAV-HEAD.js"></script>
+</body>
+</html>
