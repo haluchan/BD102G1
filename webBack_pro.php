6a157cc31bd503a9a9b645bafa09424c23193bd7
雁 <happybird.yen@gmail.com>
2017-10-14 01:58:20 +0800

    後台::商品頁 有寫註解在scss和css

--- webBack_pro.php
+++ webBack_pro.php
@@ -3,13 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>後台::商品</title>
+	<!-- 自己的css擺這裡 -->
 	<link rel="stylesheet" type="text/css" href="css/web_back_frame.css">
+	<!-- 不准動的部分，以下三行 -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="js/web_back_frame/web_back_frame.js"></script>
+	<link rel="stylesheet" href="css/webBack_pro.css">
 </head>
 <body>
 <?php require_once('web_back_frame_top.php') ?>
-<form>
+<form class="myForm">
 	<table cellspacing="1">
 		<tr>
 			<th></th>
@@ -68,6 +71,56 @@
 		</tr>
 		<tr class="tdRow">
 			<td>5</td>
+			<td>2AA</td>
+			<td>3AA</td>
+			<td>4AA</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="tdRow">
+			<td>6</td>
+			<td>2AA</td>
+			<td>3AA</td>
+			<td>4AA</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="tdRow">
+			<td>7</td>
+			<td>2AA</td>
+			<td>3AA</td>
+			<td>4AA</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="tdRow">
+			<td>8</td>
+			<td>2AA</td>
+			<td>3AA</td>
+			<td>4AA</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="tdRow">
+			<td>9</td>
+			<td>2AA</td>
+			<td>3AA</td>
+			<td>4AA</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr class="tdRow">
+			<td>10</td>
 			<td>2AA</td>
 			<td>3AA</td>
 			<td>4AA</td>
