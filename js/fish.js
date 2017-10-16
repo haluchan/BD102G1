0b619833344c23f3c67b700d142e317c60e5507c
pon <bd102pon@gmail.com>
2017-10-16 20:37:42 +0800

    表格整合+都市小農第一屏改

--- js/fish.js
+++ js/fish.js
@@ -3,77 +3,25 @@ $(function(){
   var controller = new ScrollMagic.Controller();
  
   //tweenmax.fromto(目標物件,時間)用法
-   //火箭飛入
-   var rocket= TweenMax.fromTo("#rocket", 1, {
-    x : 500,     
-        opacity : 0,
-    }, {
-       x : 0,
+
+
+
+  //Title飛入
+   var title= TweenMax.fromTo( ".fishbase #title",1,{ 
+        y:-500,    
+        opacity:0
+    },{
+       y:0,
        opacity : 1,
-       ease : Elastic.easeOut
+       ease :  Bounce.easeOut
     });
 
+  title.delay(0.1)
 
 
 
-  //Title煙霧飛入
- //   var title= TweenMax.fromTo( ".fish_universeTitle" , 2, {
- //        // transformOrigin:"right 5%",
- //        scaleX: 0.2,
- //        scaleY: 0.2,
- //        x :500, 
- //        y:-100,    
- //        opacity : 0
- //    }, {
- //       scaleX: 1,
- //       scaleY: 1,
- //       x : 0,
- //       y:0,
- //       opacity : 1,
- //       ease :  Bounce.easeOut
- //    });
-
- // title.delay(0.5)
-
-
-
-
-
-//parallax  
-var scene_star = document.getElementById('star')
-var parallax = new Parallax(scene_star)
-
-
-
-// //選購流程1-scrollMagic
-// var stepl = TweenMax.staggerFromTo(['.stepFlowLeft #one','.stepFlowLeft #two','.stepFlowLeft #three'] ,1,{
-//   y : -500,
-//   opacity : 0,
-// },{
-//    y : 0,
-//   opacity:1
-// },0.5);
-
-// var scene = new ScrollMagic.Scene({
-      
-//       triggerElement: "#trigger1",
-//       duration: 300,
-//       offset: 10,
-//       reverse: false
-//     })
-
-//     .setTween(stepl)
-//     .addIndicators({
-//             name: 'scene 01'
-//         })
-// .addTo(controller);
-
-
-
-
-
-//選購流程2-scrollMagic
-var stepr = TweenMax.staggerFromTo(['.stepFlowRight #arrow1','.stepFlowRight #arrow2','.stepFlowRight #arrow3'],0.5,{
+//選購流程-scrollMagic
+var step = TweenMax.staggerFromTo(['.stepFlowRight #arrow1','.stepFlowRight #arrow2','.stepFlowRight #arrow3'],0.5,{
   opacity:0
 },{
   opacity:1
@@ -83,19 +31,16 @@ var scene = new ScrollMagic.Scene({
       
       triggerElement: "#trigger2",
       duration: 300,
-      offset: -200,
+      offset: -300,
       reverse: false
       
     })
 
-    .setTween(stepr)
+    .setTween(step)
     // .addIndicators({
     //         name: 'scene 02'
     //     })
 .addTo(controller);
-
-
-
 
 
 
