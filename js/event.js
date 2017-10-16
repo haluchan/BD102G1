22d1585e439da1be89238b3e719a8d7b24234318
許謦懷 <beck_hsu@Becks-MacBook-Pro.local>
2017-10-16 20:46:28 +0800

    1016-2

--- js/event.js
+++ js/event.js
@@ -28,6 +28,29 @@ var controller = new ScrollMagic.Controller();
     //     })
    .addTo(controller);
 
- 
+  var tween_1 = TweenMax.staggerFromTo('.eventreco', 1, {
+        //做事情
+        opacity: 0,
+        y: 0
+    }, {
+        opacity: 1,
+        y: 0
+    },.2);
+
+
+
+    var scene_t = new ScrollMagic.Scene({
+      //做事情
+      triggerElement: "#trigger2",
+      duration: 0,
+      offset: -100,
+      reverse: false
+    
+    })
+    .setTween(tween_1)
+    // .addIndicators({
+    //         name: 'scene 01'
+    //     })
+   .addTo(controller);
      
   });
