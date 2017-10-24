3770af64d2040f1f811d4dd28d73cd946f36d776
張雅玟 <anpotiky@gmail.com>
2017-10-24 15:36:49 +0800

    refresh

--- js/hopeFarm.js
+++ js/hopeFarm.js
@@ -1,32 +1,27 @@
 $(document).ready(function(){
 $('.t1').click(function(){
-		$('.section05-g1 li').removeClass('active01');
+		$('.mark_title li').removeClass('active01');
 		$('.t1').addClass('active01');
 
-		$('.section05-g2-i1 ').removeClass('active01');
+		$('.mark_container ').removeClass('active01');
 		$('.c1').addClass('active01');
 	})
 
 	$('.t2').click(function(){
-		$('.section05-g1 li').removeClass('active01');
+		$('.mark_title li').removeClass('active01');
 		$('.t2').addClass('active01');
 
-		$('.section05-g2-i1 ').removeClass('active01');
+		$('.mark_container ').removeClass('active01');
 		$('.c2').addClass('active01');
 	})
 
 	$('.t3').click(function(){
-		$('.section05-g1 li').removeClass('active01');
+		$('.mark_title li').removeClass('active01');
 		$('.t3').addClass('active01');
 
-		$('.section05-g2-i1 ').removeClass('active01');
+		$('.mark_container ').removeClass('active01');
 		$('.c3').addClass('active01');
 	})
 
-
-
-
-	var scence = document.getElementById('scenes');
-    var parallax = new Parallax(scence);
 });
 
