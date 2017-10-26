f4be24996682c8de39ab2c13624d0061be3efb40
vivi <vivi@STU.local>
2017-10-26 11:33:12 +0800

    ok

--- php/connectGrowing_hope.php
+++ php/connectGrowing_hope.php
@@ -5,25 +5,13 @@
  * @date    2017-10-19 13:55:36
  * 
  */
-
-$dsn = "mysql:host=localhost;dbname=growing_hope;port:3306;charset=utf8";
-<<<<<<< HEAD
-
 $dsn = "mysql:host=localhost;dbname=growing_hope;port=3306;charset=utf8";
 
-=======
->>>>>>> af1a63768ae1132f46ed276913dc312f51d93da1
+
 $user = "root";
 $password = "root";
 $options = array( PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 
-<<<<<<< HEAD
 
-
-
-
-
-=======
->>>>>>> af1a63768ae1132f46ed276913dc312f51d93da1
 ?>
