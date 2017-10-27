583e53887d75094c99483b8a5f75eab82d2cea18
張雅玟 <anpotiky@gmail.com>
2017-10-27 21:45:59 +0800

    update

--- js/hopeFarm.js
+++ js/hopeFarm.js
@@ -27,13 +27,61 @@ $('.t1').click(function(){
 	})
 
 	$('.t4').click(function(){
-		$('hopefarmrwd-select li').removeClass('active01');
+		$('mark_title li').removeClass('active01');
 		$('.t4').addClass('active01');
 
 		$('.mark_container ').removeClass('active01');
 		$('.c4').addClass('active01');
 		
 	})
+
+	// RWD
+	$('.t4').click(function(){
+		$('.hopefarmrwd-select li').removeClass('active01');
+		$('.t4').addClass('active01');
+
+		$('.mark_container ').removeClass('active01');
+		$('.c4').addClass('active01');
+
+		
+		
+	})
+
+	$('.t1').click(function(){
+		$('.hopefarmrwd-select li').removeClass('active01');
+		$('.t1 ').addClass('active01');
+
+		$('.mark_container ').removeClass('active01');
+		$('.c1').addClass('active01');
+		
+	})
+
+	$('.t2').click(function(){
+		$('.hopefarmrwd-select li').removeClass('active01');
+		$('.t2').addClass('active01');
+
+		$('.mark_container ').removeClass('active01');
+		$('.c2').addClass('active01');
+		
+	})
+
+
+	$('.t3').click(function(){
+		$('.hopefarmrwd-select li').removeClass('active01');
+		$('.t3').addClass('active01');
+
+		$('.mark_container ').removeClass('active01');
+		$('.c3').addClass('active01');
+		
+	})
+
+
+	$('.hopefarmrwd-select a').click(function(){
+		$('html ,body').animate({
+			scrollTop:$($.attr(this,'href')).offset().top-110
+		},700);//速度
+	})
+	
 
 });
 
