9a6b14c2b5da490a4ab7e7061b978ce4008fce6e
Halu <forchang0120@gmail.com>
2017-10-28 21:52:00 +0800

    fix bug

--- php/login-program.php
+++ php/login-program.php
@@ -49,7 +49,9 @@ try {
 		$_SESSION["mem_gender"] = $memRow->mem_gender;
 		$_SESSION["mem_add"] = $memRow->mem_add;
 		$_SESSION["mem_phone"] = $memRow->mem_phone;
-		$_SESSION["mem_bonus"] = $memRow->mem_bonus;
+		$_SESSION["mem_bonus"] = $memRow->mem_bonus; 
+		$_SESSION["mem_seed"] = $memRow->mem_mem_seed;
+		$_SESSION["mem_psw"] = $memRow->mem_psw;
 		echo $memRow->mem_name;
    
 
