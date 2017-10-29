40c833dc0c18a0de9265a6581759de20d7389f32
Chin-lin Chung <chin-linchung@Chin-lins-MacBook-Pro.local>
2017-10-29 20:40:02 +0800

    update

--- activitySend.php
+++ activitySend.php
@@ -11,11 +11,11 @@
 			require_once('php/connectBeck.php');
 			$sql = "insert into activity(act_name,act_date,act_loc,act_limit, act_price,act_txt,act_start,act_end) values(:act_name,:act_date,:act_loc,:act_limit,:act_price,:act_txt,:act_start,:act_end)";
 			// $sql2 = "insert into activityPhoto(act_no,pho_no) values(:act_no,:pho_no);"
-			
+			$sqlImage = "select act_no from activity join activityPhoto on activity.act_no = activityPhoto.act_no"; 
 
 
 			$activity = $pdo->prepare($sql);
-			$activity = $pdo->prepare($sql2);
+			$activity_Pho = $pdo->prepare($sqlImage);
 			$activity->bindValue("act_name",$_REQUEST["act_name"]);
 			$activity->bindValue(":act_date",$_REQUEST["act_date"]);
 			$activity->bindValue(":act_loc",$_REQUEST["act_loc"]);
@@ -24,10 +24,10 @@
 			$activity->bindValue(":act_txt",$_REQUEST["act_txt"]);
 			$activity->bindValue(":act_start",$_REQUEST["act_start"]);
 			$activity->bindValue(":act_end",$_REQUEST["act_end"]);
-			$activity->bindValue(":pho_no",$_REQUEST[""])
+			$activity->bindValue(":pho_no",$_REQUEST["photo"])
 			$activity->execute();
-			$actRow = $activity->fetchObject();
-			$actRow-> act_no
+
+
 		} catch (PDOException $e) {
 			echo "錯誤行號: ", $e-> getLine(), "<br>";
 			echo "錯誤訊息: ", $e-> getMessage(), "<br>";
